d5767128c0ce4c772ebda4256c8e339d28a49004
jennannharris <114770441+jennannharris@users.noreply.github.com>
2023-03-05 11:39:06 -0500

    3.5.23

--- db/db.js
+++ db/db.js
@@ -2,9 +2,23 @@ import Sequelize from "sequelize";
 import PostModel from "./Post.js";
 import UserModel from "./User.js";
 
-const db = new Sequelize("postgres://jenniferharris@localhost:5432/capstone", {
-  logging: false,
-});
+
+let db;
+if (process.env.RDS_HOSTNAME) {
+	console.log("Connecting to RDS", process.env.RDS_HOSTNAME);
+	// if we're running on elasticbeanstalk, use elasticbeanstalk connection
+	db = new Sequelize(`postgres://${process.env.RDS_USERNAME}:${process.env.RDS_PASSWORD}@${process.env.RDS_HOSTNAME}:${process.env.RDS_PORT}/${process.env.RDS_DB_NAME}`, {
+		logging: false,
+	})
+} else {
+	console.log("Connecting to local database");
+	// if we're running locally, use the localhost connection
+	db = new Sequelize("postgres://hackupstate@localhost:5432/blog", {
+		logging: false,
+	});
+}
+
+
 const Post = PostModel(db);
 const User = UserModel(db);
 
@@ -13,7 +27,7 @@ const connectToDB = async () => {
     await db.authenticate();
     console.log("Connected to DB successfully");
 
-    db.sync({ force: false });
+   await db.sync({ force: false });
   } catch (error) {
     console.error(error);
     console.error("PANIC! DB POBLEM!");
